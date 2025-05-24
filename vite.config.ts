aac2b095c24578739cb7404af512cf669dc3633c
JunyiLye <junyilye@gmail.com>
2025-05-24 22:34:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Pokedex-Website',
+  base: '/Pokedex-Website/',
   plugins: [
     vue(),
     vueDevTools(),
